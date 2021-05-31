9c2ef4520017ceef0497f82a2616ac8006c4e2aa
Naveen12345-alt <naveenchauhan1234@protonmail.com>
2021-05-31 23:09:36 +0530

    browse:SignOut+Hero:Completed

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import { Link as ReactRouterLink } from 'react-router-dom'
-import { Background, Container, Logo, ButtonLink } from './styles/header'
+import {
+  Background,
+  Container,
+  Logo,
+  ButtonLink,
+  Feature,
+  Text,
+  Dropdown,
+  FeatureCallOut,
+  Link,
+  Group,
+  Picture,
+  Profile,
+} from './styles/header'
 
 export default function Header({ bg = true, children, ...restProps }) {
   return bg ? <Background {...restProps}>{children}</Background> : children
@@ -10,8 +23,36 @@ Header.Frame = function HeaderFrame({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>
 }
 
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+  return <Group {...restProps}>{children}</Group>
+}
+
 Header.Button = function Button({ children, ...restProps }) {
   return <ButtonLink {...restProps}>{children}</ButtonLink>
+}
+
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+  return <Feature {...restProps}>{children}</Feature>
+}
+
+Header.Profile = function HeaderProfile({ children, ...restProps }) {
+  return <Profile {...restProps}>{children}</Profile>
+}
+
+Header.Picture = function HeaderPicture({ src, ...restProps }) {
+  return <Picture {...restProps} src={`/images/users/${src}.png`} />
+}
+
+Header.Text = function HeaderText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>
+}
+
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+  return <Dropdown {...restProps}>{children}</Dropdown>
+}
+
+Header.TextLink = function HeaderTextLink({ children, ...restProps }) {
+  return <Link {...restProps}>{children}</Link>
 }
 
 Header.Logo = function HeaderLogo({ to, ...restProps }) {
@@ -20,4 +61,11 @@ Header.Logo = function HeaderLogo({ to, ...restProps }) {
       <Logo {...restProps} />
     </ReactRouterLink>
   )
+}
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({
+  children,
+  ...restProps
+}) {
+  return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>
 }
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -2,6 +2,8 @@ import React, { useState, useContext, useEffect } from 'react'
 import { SelectProfileContainer } from './profiles'
 import { FirebaseContext } from '../context/firebase'
 import { Header, Loading } from '../components'
+import * as ROUTES from '../constants/routes'
+import logo from '../logo.svg'
 
 export function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({})
@@ -20,8 +22,49 @@ export function BrowseContainer({ slides }) {
     <React.Fragment>
       {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
 
-      <Header src="joker1">
-        <p>Hello</p>
+      <Header src="joker1" dontShowOnSmallViewPort>
+        <Header.Frame>
+          <Header.Group>
+            <Header.Logo to={ROUTES.HOME} alt="Netflix" src={logo} />
+            <Header.TextLink>Films</Header.TextLink>
+            <Header.TextLink>Series</Header.TextLink>
+          </Header.Group>
+          <Header.Group>
+            <Header.Profile>
+              <Header.Picture src={user.photoURL} />
+              <Header.Dropdown>
+                <Header.Group>
+                  <Header.Picture src={user.photoURL} />
+                  <Header.TextLink>{user.displayName}</Header.TextLink>
+                </Header.Group>
+                <Header.Group>
+                  <Header.TextLink onClick={() => firebase.auth().signOut()}>
+                    Sign Out
+                  </Header.TextLink>
+                </Header.Group>
+              </Header.Dropdown>
+            </Header.Profile>
+          </Header.Group>
+        </Header.Frame>
+        <Header.Feature>
+          <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+          <Header.Text>
+            Arthur sneaks into a private movie theater event and confronts
+            Thomas, who tells him that Penny is delusional and not his
+            biological mother. In denial, Arthur visits Arkham State Hospital
+            and steals Penny's file; the file states she adopted Arthur while
+            working as a nurse at the hospital in the 1950s. Penny then raised
+            Arthur along with her abusive boyfriend, who was later arrested and
+            died in jail. Although Penny claims that she had an affair with
+            Thomas, it is hinted to have been a lie fabricated in order to
+            blackmail him into paying alimony, a scheme that was ultimately
+            unsuccessful. Distraught, Arthur returns home and enters Sophie's
+            apartment unannounced. Frightened, Sophie tells him he is in the
+            wrong apartment and asks him to leave; it is then revealed that
+            their relationship was one of Arthur's delusions. The following day,
+            Arthur kills Penny at the hospital by smothering her with a pillow.
+          </Header.Text>
+        </Header.Feature>
       </Header>
     </React.Fragment>
   ) : (
